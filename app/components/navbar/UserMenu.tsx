b2d8aa78e1af96e471fbf6cf88132a33220d3729
Luciano Ferrari <luciano@bankuish.com>
2023-06-21 00:01:04 -0300

    avatar bar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+
 const UserMenu = () => {
     return (
         <div className="relative">
@@ -20,6 +23,31 @@ const UserMenu = () => {
           "
                 >
                     Airbnb your home
+                </div>
+                <div
+                    onClick={() => { }}
+                    className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          "
+                >
+                    <AiOutlineMenu />
+
+                    <div className="hidden md:block">
+                    <Avatar src="" />
+
+                    </div>
                 </div>
             </div>
         </div>
