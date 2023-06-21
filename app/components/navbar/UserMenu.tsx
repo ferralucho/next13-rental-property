a35cbf7797ecf89501fe30db26b8c24e5d05a871
Luciano Ferrari <luciano@bankuish.com>
2023-06-21 00:14:51 -0300

    menu item

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,21 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from 'react';
+import { useRouter } from "next/navigation";
+import MenuItem from "./MenuItem";
+
 
 const UserMenu = () => {
+    const router = useRouter();
+
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -25,7 +38,7 @@ const UserMenu = () => {
                     Airbnb your home
                 </div>
                 <div
-                    onClick={() => { }}
+                    onClick={toggleOpen}
                     className="
           p-4
           md:py-1
@@ -45,11 +58,72 @@ const UserMenu = () => {
                     <AiOutlineMenu />
 
                     <div className="hidden md:block">
-                    <Avatar src="" />
+                        <Avatar src="" />
 
                     </div>
                 </div>
             </div>
+
+
+            {isOpen && (
+                <div
+                    className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        {true ? (
+                            <>
+                                <MenuItem
+                                    label="My trips"
+                                    onClick={() => router.push('/trips')}
+                                />
+                                <MenuItem
+                                    label="My favorites"
+                                    onClick={() => router.push('/favorites')}
+                                />
+                                <MenuItem
+                                    label="My reservations"
+                                    onClick={() => router.push('/reservations')}
+                                />
+                                <MenuItem
+                                    label="My properties"
+                                    onClick={() => router.push('/properties')}
+                                />
+                                <MenuItem
+                                    label="Airbnb your home"
+                                    onClick={() => { }}
+                                />
+                                <hr />
+                                <MenuItem
+                                    label="Logout"
+                                    onClick={() => { }}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <MenuItem
+                                    label="Login"
+                                    onClick={() => { }}
+                                />
+                                <MenuItem
+                                    label="Sign up"
+                                    onClick={() => { }}
+                                />
+                            </>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
