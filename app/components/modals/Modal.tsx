52c5b50f928485e20c69aac02f984aa111302c6c
Luciano Ferrari <luciano@bankuish.com>
2023-06-27 16:54:03 -0300

    modal changes

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+
 import Button from "../Button";
 
 interface ModalProps {
@@ -52,7 +53,7 @@ const Modal: React.FC<ModalProps> = ({
         }
 
         onSubmit();
-    }, [disabled, onSubmit]);
+    }, [onSubmit, disabled]);
 
     const handleSecondaryAction = useCallback(() => {
         if (disabled || !secondaryAction) {
@@ -70,41 +71,41 @@ const Modal: React.FC<ModalProps> = ({
         <>
             <div
                 className="
-            justify-center 
-            items-center 
-            flex 
-            overflow-x-hidden 
-            overflow-y-auto 
-            fixed 
-            inset-0 
-            z-50 
-            outline-none 
-            focus:outline-none
-            bg-neutral-800/70
-          "
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
+          focus:outline-none
+          bg-neutral-800/70
+        "
             >
                 <div className="
-            relative 
-            w-full
-            md:w-4/6
-            lg:w-3/6
-            xl:w-2/5
-            my-6
-            mx-auto 
-            h-full 
-            lg:h-auto
-            md:h-auto
-            "
+          relative 
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/5
+          my-6
+          mx-auto 
+          h-full 
+          lg:h-auto
+          md:h-auto
+          "
                 >
                     {/*content*/}
-          <div className={`
+                    <div className={`
             translate
             duration-300
             h-full
             ${showModal ? 'translate-y-0' : 'translate-y-full'}
             ${showModal ? 'opacity-100' : 'opacity-0'}
           `}>
-            <div className="
+                        <div className="
               translate
               h-full
               lg:h-auto
@@ -120,9 +121,9 @@ const Modal: React.FC<ModalProps> = ({
               outline-none 
               focus:outline-none
             "
-            >
-              {/*header*/}
-              <div className="
+                        >
+                            {/*header*/}
+                            <div className="
                 flex 
                 items-center 
                 p-6
@@ -131,9 +132,9 @@ const Modal: React.FC<ModalProps> = ({
                 relative
                 border-b-[1px]
                 "
-              >
-                <button
-                  className="
+                            >
+                                <button
+                                    className="
                     p-1
                     border-0 
                     hover:opacity-70
@@ -141,50 +142,49 @@ const Modal: React.FC<ModalProps> = ({
                     absolute
                     left-9
                   "
-                  onClick={handleClose}
-                >
-                  <IoMdClose size={18} />
-                </button>
-                <div className="text-lg font-semibold">
-                  {title}
-                </div>
-              </div>
-              {/*body*/}
-              <div className="relative p-6 flex-auto">
-                {body}
-              </div>
-              {/*footer*/}
-              <div className="flex flex-col gap-2 p-6">
-                <div 
-                  className="
+                                    onClick={handleClose}
+                                >
+                                    <IoMdClose size={18} />
+                                </button>
+                                <div className="text-lg font-semibold">
+                                    {title}
+                                </div>
+                            </div>
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
+                                {body}
+                            </div>
+                            {/*footer*/}
+                            <div className="flex flex-col gap-2 p-6">
+                                <div
+                                    className="
                     flex 
                     flex-row 
                     items-center 
                     gap-4 
                     w-full
                   "
-                >
-                  {secondaryAction && secondaryActionLabel && (
-                    <Button 
-                      disabled={disabled} 
-                      label={secondaryActionLabel} 
-                      onClick={handleSecondaryAction}
-                      outline
-                    />  
-                  )}
-                  <Button 
-                    disabled={disabled} 
-                    label={actionLabel} 
-                    onClick={handleSubmit}
-                  />
+                                >
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button
+                                            disabled={disabled}
+                                            label={secondaryActionLabel}
+                                            onClick={handleSecondaryAction}
+                                            outline
+                                        />
+                                    )}
+                                    <Button
+                                        disabled={disabled}
+                                        label={actionLabel}
+                                        onClick={handleSubmit}
+                                    />
+                                </div>
+                                {footer}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                {footer}
-              </div>
             </div>
-          </div>
-                </div>
-            </div>
-
         </>
     );
 }
